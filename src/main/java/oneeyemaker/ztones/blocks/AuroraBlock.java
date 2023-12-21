3b5ec1d5877ff29f96dd0d97d0048c32f39d2f82
Alexander Anishin <oneeyemaker@gmail.com>
2023-12-21 12:02:46 +0300

    Added Aurora block

---
+++ src/main/java/oneeyemaker/ztones/blocks/AuroraBlock.java
@@ -0,0 +1,32 @@
+package oneeyemaker.ztones.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import oneeyemaker.ztones.Tags;
+import oneeyemaker.ztones.Ztones;
+
+public class AuroraBlock extends Block {
+
+    public AuroraBlock() {
+        super(Material.rock);
+        this.setHardness(1.5f);
+        this.setResistance(12.0f);
+        this.setStepSound(Block.soundTypeSnow);
+        this.setCreativeTab(Ztones.creativeTab);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister) {
+        this.blockIcon = iconRegister.registerIcon(String.format("%s:aurora", Tags.MODID));
+    }
+
+    @Override
+    public String getUnlocalizedName() {
+        return String.format("%s.aurora", Tags.MODID);
+    }
+}
--- src/main/java/oneeyemaker/ztones/blocks/ModBlocks.java
+++ src/main/java/oneeyemaker/ztones/blocks/ModBlocks.java
@@ -8,6 +8,7 @@ import oneeyemaker.ztones.items.ZtoneGenericItemBlock;
 
 public class ModBlocks {
 
+    public static AuroraBlock auroraBlock;
     private static final HashMap<ZtoneType, ZtoneGenericBlock> genericBlocks = new HashMap<>();
 
     public static ZtoneGenericBlock getBlock(ZtoneType type) {
@@ -15,6 +16,8 @@ public class ModBlocks {
     }
 
     public static void registerBlocks() {
+        auroraBlock = new AuroraBlock();
+        GameRegistry.registerBlock(auroraBlock, "auroraBlock");
         for (ZtoneType type : ZtoneType.values()) {
             if (type.isEnabled()) {
                 ZtoneGenericBlock ztoneGenericBlock = new ZtoneGenericBlock(type);
